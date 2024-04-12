5399ab110e85b434fcd0ca10ca78dd86cfff410d
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2024-04-12 15:00:28 +0200

    index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts">
-//
+<script setup lang="ts" >
+import { ref } from 'vue'
+const sectionOpen = ref(1)
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <h1 class="text-2xl">Bonjour tout le monde !</h1>
+  <section>
+  <btn class="text-xl" @pointerdown="sectionOpen = 1">bouton 1</btn>
+  <p v-show="sectionOpen === 1">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</section>
+<section>
+  <btn class="text-xl" @pointerdown="sectionOpen = 2">bouton 2</btn>
+  <p v-show="sectionOpen === 2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</section>
+<section>
+  <btn class="text-xl" @pointerdown="sectionOpen = 3">bouton 3</btn>
+  <p v-show="sectionOpen === 3">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</section>
 </template>
