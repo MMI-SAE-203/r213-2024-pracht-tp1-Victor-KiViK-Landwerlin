09c15009aa4f737caf526806226c2c56cc9cb216
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2024-04-12 14:33:03 +0200

    x show

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 const menuIsOpen = ref(false)
@@ -14,13 +13,14 @@ onErrorCaptured((err, instance, info) => {
 <template>
   <header>
     <button
+    @pointerdown="menuIsOpen = !menuIsOpen"
     aria-controls="mainNav"
     aria-expanded="true"
     class="rounded-full border-2 border-red-600 bg-red-300 px-2">
     menu
     </button>
-  <nav id="mainNav">
-    <ul>
+  <nav id="mainNav" v-show="menuIsOpen">
+    <ul >
       <li><RouterLink to="/">item 1</RouterLink></li>
       <li><RouterLink to="/">item 2</RouterLink></li>
       <li><RouterLink to="/">item 3</RouterLink></li>
