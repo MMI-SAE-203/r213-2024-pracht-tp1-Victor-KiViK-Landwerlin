3252cefd999bc959c0b1e54b09904c25c8dcb192
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2024-04-12 14:35:45 +0200

    transiyion

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,20 @@ onErrorCaptured((err, instance, info) => {
     class="rounded-full border-2 border-red-600 bg-red-300 px-2">
     menu
     </button>
-  <nav id="mainNav" v-show="menuIsOpen">
-    <ul >
-      <li><RouterLink to="/">item 1</RouterLink></li>
-      <li><RouterLink to="/">item 2</RouterLink></li>
-      <li><RouterLink to="/">item 3</RouterLink></li>
-    </ul>
-  </nav>
+    <Transition
+    class="transition-transform duration-1000"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="-translate-x-full"
+  >
+      <nav id="mainNav" v-show="menuIsOpen">
+        <ul >
+          <li><RouterLink to="/">item 1</RouterLink></li>
+          <li><RouterLink to="/">item 2</RouterLink></li>
+          <li><RouterLink to="/">item 3</RouterLink></li>
+        </ul>
+      </nav>
+    </Transition>
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
